5361f091944be358d6fd14907d69d356ecdfb1a5
MKowoj <abc11yz@gmail.com>
2025-05-09 00:52:25 -0600

    Primer commit del proyecto Auditoren

---
+++ auditoren-react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ReactivoTable from './ReactivoTable'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Auditoría de protección de datos personales</h1>
+      <ReactivoTable />
+    </div>
+  )
+}
+
+export default App
